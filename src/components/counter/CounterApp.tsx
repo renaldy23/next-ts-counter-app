8cf0792d5db00c43a0e28825d79343f9a2874817
mrenaldyr <mrenaldyr03@gmail.com>
2023-11-19 19:07:20 +0700

    feat: add counter functionality

--- src/components/counter/CounterApp.tsx
+++ src/components/counter/CounterApp.tsx
@@ -1,18 +1,24 @@
+'use client';
+
+import { useState } from 'react';
 import { Button } from '../ui/button';
 
 export const CounterApp = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="flex gap-4 items-center">
       <Button
         type="button"
-        text="Increment"
-        className="px-4 py-1 rounded-md bg-blue-400 hover:bg-blue-500 transition-colors duration-150 text-white"
-      />
-      <p>1</p>
-      <Button
-        type="button"
         text="Decrement"
         className="px-4 py-1 rounded-md bg-blue-400 hover:bg-blue-500 transition-colors duration-150 text-white"
+        onClick={() => setCounter(counter - 1)}
+      />
+      <p>{counter}</p>
+      <Button
+        type="button"
+        text="Increment"
+        className="px-4 py-1 rounded-md bg-blue-400 hover:bg-blue-500 transition-colors duration-150 text-white"
+        onClick={() => setCounter(counter + 1)}
       />
     </div>
   );
